2eab78ca2515d1bf34da1c2c6e58505751b6c219
lu chao <1457065670@qq.com>
2022-07-26 22:37:59 +0800

    暴力解法

--- leetcode/src/main/java/Solution22.java
+++ leetcode/src/main/java/Solution22.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @Description: 22 括号
@@ -8,32 +9,50 @@ import java.util.List;
  * @Version: v1.0
  */
 class Solution22 {
-    final String LEFT="(",RIGHT=")";
-    private List<String> list=new ArrayList<>();
+    final String LEFT = "(", RIGHT = ")";
+    private List<String> list = new ArrayList<>();
+
     public List<String> generateParenthesis(int n) {
-        StringBuilder tmp=new StringBuilder();
-        generate(0,n, tmp);
+        String tmp = "";
+        generate(0, 0, 0, n * 2, tmp);
         return list;
     }
 
-    private void generate(int count,int n,StringBuilder tmp){
-        if(count>=n){
-            if(isValidate(tmp.toString())){
-
-            }
-            return ;
+    private void generate(int count, int leftCount, int rightCount, int n, String tmp) {
+        if (leftCount > n / 2) {
+            return;
         }
 
-        generate(count+1,n,tmp.append(LEFT));
-        generate(count+1,n,tmp.append(RIGHT));
+        if (rightCount > n / 2) {
+            return;
+        }
+
+        if (count >= n) {
+            String str = tmp;
+            if (isValidate(str)) {
+                list.add(str);
+            }
+            return;
+        }
+
+        generate(count + 1, leftCount + 1, rightCount, n, tmp + LEFT);
+        generate(count + 1, leftCount, rightCount + 1, n, tmp + RIGHT);
     }
 
     private boolean isValidate(String str) {
-        
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (!stack.empty() && str.charAt(i) == ')' && stack.peek() == '(') {
+                stack.pop();
+                continue;
+            }
+            stack.push(str.charAt(i));
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
         Solution22 solution22 = new Solution22();
-        System.out.println(solution22.generateParenthesis(3));
+        System.out.println(solution22.generateParenthesis(8));
     }
 }
