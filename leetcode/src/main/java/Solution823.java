0a47a106c398d3a641a7b5c36972f28cc9f84313
luchao <1457065670@qq.com>
2022-07-28 17:38:05 +0800

    leetcode 823带因子的二叉树

---
+++ leetcode/src/main/java/Solution823.java
@@ -0,0 +1,11 @@
+/**
+ * @Description: 带因子的二叉树
+ * @Auther: LuChao
+ * @Date: 2022/7/28 14:38
+ * @Version: v1.0
+ */
+public class Solution823 {
+    public int numFactoredBinaryTrees(int[] arr) {
+        return 0;
+    }
+}
