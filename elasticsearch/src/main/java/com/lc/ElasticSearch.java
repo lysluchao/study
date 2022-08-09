c4f0f37202e16906efce3b1ce81fab1f08452327
luchao <1457065670@qq.com>
2022-08-09 11:32:43 +0800

    es启动类

---
+++ elasticsearch/src/main/java/com/lc/ElasticSearch.java
@@ -0,0 +1,17 @@
+package com.lc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: es主类
+ * @Auther: LuChao
+ * @Date: 2022/8/9 11:31
+ * @Version: v1.0
+ */
+@SpringBootApplication
+public class ElasticSearch {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearch.class, args);
+    }
+}
