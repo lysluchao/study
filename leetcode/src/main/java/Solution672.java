31e88f8e5d6e170b29834778107b140a10ceb32a
luchao <1457065670@qq.com>
2022-09-16 18:04:50 +0800

    leetcode672 位运算实现

---
+++ leetcode/src/main/java/Solution672.java
@@ -0,0 +1,24 @@
+/**
+ * 灯泡开关
+ *
+ * 使用位运算
+ *
+ * @author luchao
+ * @version v1.0
+ * @date 2022/9/16 16:42
+ */
+public class Solution672 {
+    int lights;
+
+    public int flipLight(int n, int presses) {
+        for (int i = 0; i <; i++) {
+            lights += 1 << i;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution672 solution672 = new Solution672();
+        solution672.flipLight(5, 0);
+    }
+}
