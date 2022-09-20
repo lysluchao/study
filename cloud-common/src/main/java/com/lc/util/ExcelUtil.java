668c28f743bb8e4e12c5173bd5274beda39ff9c7
luchao <1457065670@qq.com>
2022-09-20 17:57:48 +0800

    ExcelUtil

--- cloud-common/src/main/java/com/lc/util/ExcelUtil.java
+++ cloud-common/src/main/java/com/lc/util/ExcelUtil.java
@@ -7,9 +7,17 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.function.Supplier;
 
 /**
- * @Description: Excel工具类，POI实现
+ * Excel工具类，POI实现
+ *
  * @Auther: LuChao
  * @Date: 2022/8/11 11:12
  * @Version: v1.0
@@ -19,22 +27,37 @@ public class ExcelUtil {
     /**
      * 创建excel文档
      *
-     * @param path 存储路径
-     * @param workbook  excel文档
+     * @param path     存储路径
+     * @param workbook excel文档
      * @throws IOException 路径无效时
      */
-    public static void createExcel(HSSFWorkbook workbook,String path) throws IOException {
+    public static void createExcelBySuppliedData(HSSFWorkbook workbook, String path) throws IOException {
 
         if (StringUtil.isBlank(path)) {
             throw new IllegalArgumentException("路径错误");
         } else {
-            File file=new File(path);
-            if(!file.exists()){
-                file.createNewFile();
+            File file = new File(path);
+            if (!file.exists()) {
+                boolean createSuccess = false;
+                for (int i = 0; i < 3 && !createSuccess; i++) {
+                    createSuccess = file.createNewFile();
+                }
             }
             workbook.write(file);
             workbook.close();
         }
+    }
+
+    /**
+     * 创建excel文档
+     *
+     * @param suppliedData excel中的数据
+     * @param path         存储路径
+     * @throws IOException 路径无效时
+     */
+    public static <T extends Iterable<?>> void createExcelBySuppliedData(Supplier<T> suppliedData, LinkedHashMap<String, String> sortedCol, String path) throws IOException {
+        HSSFWorkbook workbook = createWorkbookBySuppliedData(suppliedData, sortedCol);
+        createExcelBySuppliedData(workbook, path);
     }
 
     /**
@@ -53,16 +76,115 @@ public class ExcelUtil {
     }
 
     /**
-     * 设置表头
-     * @param sheet 表单
-     * @param header 标题头
+     * 根据提供的数据导出WorkBook
+     *
+     * @param suppliedData 提供的数据
+     * @param sheetNames   sheet名称
+     * @param sheetSize    sheet大小
+     * @param <T>          单条数据类型
+     * @return 创建的workbook
      */
-    public static void initSheetHeader(HSSFSheet sheet,String ... header){
-        HSSFRow row = sheet.createRow(0);
-        for (int i = 0; i < header.length; i++) {
-            HSSFCell cell = row.createCell(i);
-            cell.setCellValue(header[i]);
+    public static <T extends Iterable<?>> HSSFWorkbook createWorkbookBySuppliedData(Supplier<T> suppliedData, String[] sheetNames, int sheetSize, LinkedHashMap<String, String> sortedCol) {
+
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        T t = suppliedData.get();
+        Iterator<?> iterator = t.iterator();
+        for (String sheetName : sheetNames) {
+            try {
+                createSheet(sheetSize, workbook, iterator, sheetName, sortedCol);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
         }
+        return workbook;
+    }
+
+    public static void createSheet(int sheetSize, HSSFWorkbook workbook, Iterator<?> iterator, String sheetName, LinkedHashMap<String, String> sortedCol) throws IllegalAccessException, InvocationTargetException {
+        HSSFSheet sheet = createSheet(workbook, sheetName);
+
+        createHeader(sheet, sortedCol);
+
+        HashMap<String, Integer> colIndexMap = initCilIndexMap(sortedCol);
+
+        writeSheet(sheetSize, iterator, sheet, colIndexMap);
+    }
+
+    /**
+     * 写入sheet的值
+     *
+     * @param sheetSize   sheet大小
+     * @param iterator    数据迭代器
+     * @param sheet       待写入的sheet
+     * @param colIndexMap 字段写入顺序
+     */
+    private static void writeSheet(int sheetSize, Iterator<?> iterator, HSSFSheet sheet, HashMap<String, Integer> colIndexMap) {
+        for (int i = 1; i < sheetSize && iterator.hasNext(); i++) {
+            Object data = iterator.next();
+            Class<?> clazz = data.getClass();
+            Method[] methods = clazz.getMethods();
+
+            HSSFRow row = sheet.createRow(i);
+
+            for (Method method : methods) {
+                String methodName = method.getName();
+                if (methodName.startsWith("get") && method.getReturnType() == String.class && colIndexMap.get(methodName.substring(3)) != null) {
+                    String val = null;
+                    try {
+                        val = (String) method.invoke(data);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                    HSSFCell cell = row.createCell(colIndexMap.get(methodName.substring(3)));
+                    cell.setCellValue(val);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取字段放置的顺序关系
+     *
+     * @param sortedCol field和中文名的映射
+     * @return 顺序关系
+     */
+    private static HashMap<String, Integer> initCilIndexMap(LinkedHashMap<String, String> sortedCol) {
+        Object[] objects = sortedCol.keySet().toArray();
+        HashMap<String, Integer> colIndexMap = new HashMap<>();
+        for (int i = 0; i < objects.length; i++) {
+            colIndexMap.put((String) objects[i], i);
+        }
+        return colIndexMap;
+    }
+
+    /**
+     * 创建表头
+     *
+     * @param sheet     数据表
+     * @param sortedCol 表头行
+     */
+    public static void createHeader(HSSFSheet sheet, LinkedHashMap<String, String> sortedCol) {
+        HSSFRow row = sheet.createRow(0);
+
+        Collection<String> values = sortedCol.values();
+        Object[] valuesArray = values.toArray();
+        for (int i = 0; i < valuesArray.length; i++) {
+            HSSFCell cell = row.createCell(i);
+            cell.setCellValue((String) valuesArray[i]);
+        }
+    }
+
+
+    /**
+     * 根据提供的数据导出WorkBook
+     *
+     * @param suppliedData 提供的数据
+     * @param <T>          单条数据类型
+     * @return 创建的workbook
+     */
+    public static <T extends Iterable<?>> HSSFWorkbook createWorkbookBySuppliedData(Supplier<T> suppliedData, LinkedHashMap<String, String> sortedCol) {
+        return createWorkbookBySuppliedData(suppliedData, new String[]{"default"}, Integer.MAX_VALUE, sortedCol);
     }
 
 }
---
+++ cloud-common/src/main/test/com/lc/util/ExcelUtilTest.java
@@ -0,0 +1,69 @@
+package com.lc.util;
+
+import com.lc.common.entities.BankBranchEntity;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ExcelUtilTest {
+
+    @org.junit.Test
+    public void createExcelBySuppliedData() {
+    }
+
+    @org.junit.Test
+    public void testCreateExcelBySuppliedData() {
+    }
+
+    @org.junit.Test
+    public void createSheet() {
+    }
+
+    @org.junit.Test
+    public void initSheetHeader() {
+    }
+
+    @org.junit.Test
+    public void createWorkbookBySuppliedData() {
+    }
+
+    @org.junit.Test
+    public void testCreateSheet() {
+    }
+
+    @org.junit.Test
+    public void testCreateWorkbookBySuppliedData() {
+    }
+
+    @Test
+    public void createBook() throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        ExcelUtil.createSheet(workbook, "a");
+        ExcelUtil.createExcelBySuppliedData(workbook, "D://a.xls");
+    }
+
+    @Test
+    public void createExcel() throws IOException {
+
+        LinkedHashMap<String, String> col = new LinkedHashMap<>();
+        col.put("Bankcode", "内部行号");
+        col.put("Branchcode", "联行号");
+        col.put("Bankcodeunit", "联行号单位");
+        col.put("Eisbnkcode", "电子联行号");
+        ExcelUtil.createExcelBySuppliedData(() -> {
+            List<BankBranchEntity> bankBranchEntities = new ArrayList<>();
+            BankBranchEntity bankBranchEntity = new BankBranchEntity();
+            bankBranchEntity.setBankcode("1111");
+            bankBranchEntity.setBranchcode("2222");
+            bankBranchEntity.setBankcodeunit("3333");
+            bankBranchEntity.setEisbnkcode("4444");
+            bankBranchEntities.add(bankBranchEntity);
+            return bankBranchEntities;
+        }, col, "D://a" + System.currentTimeMillis() + ".xls");
+    }
+
+}
