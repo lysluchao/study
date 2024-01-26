3e1cb8d721f81bd6e2173be781e1aeabadfe94e6
chao14.lu <chao14.lu@tcl.com>
2024-01-26 15:16:02 +0800

    seata

--- cloud-payment-8001/src/main/java/com/lc/payment/service/impl/PaymentServiceImpl.java
+++ cloud-payment-8001/src/main/java/com/lc/payment/service/impl/PaymentServiceImpl.java
@@ -4,6 +4,7 @@ package com.lc.payment.service.impl;
 import com.lc.common.entities.Payment;
 import com.lc.payment.dao.PaymentDao;
 import com.lc.payment.service.PaymentService;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,6 +15,7 @@ public class PaymentServiceImpl implements PaymentService {
     private PaymentDao paymentDao;
 
     @Override
+    @GlobalTransactional(rollbackFor = Exception.class)
     public int create(Payment payment) {
         return paymentDao.create(payment);
     }
